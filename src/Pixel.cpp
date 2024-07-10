c22016abe017b7d155e84ead9b985e9746b3706a
Hícaro Vitor <hicaro.abreu@ufv.br>
2024-07-10 07:10:43 -0300

    feat: add pixel class

---
+++ src/Pixel.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Eu on 10/07/2024.
+//
+
+#include "Pixel.h"
---
+++ src/Pixel.h
@@ -0,0 +1,16 @@
+//
+// Created by Eu on 10/07/2024.
+//
+
+#ifndef PIXEL_H
+#define PIXEL_H
+
+
+
+class Pixel {
+
+};
+
+
+
+#endif //PIXEL_H

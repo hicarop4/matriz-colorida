0e70f3dd7ac1b48d5a59e5dd2468c897b3db662f
Hícaro Vitor <hicaro.abreu@ufv.br>
2024-09-26 18:23:47 -0300

    add convert to ppm.py file to help generate B&W images in ppm format

---
+++ cmake-build-debug/CMakeFiles/TRABALHO.dir/DependInfo.cmake
@@ -0,0 +1,27 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Users/Eu/Documents/matriz-de-distancias/Calculadora.cpp" "CMakeFiles/TRABALHO.dir/Calculadora.cpp.obj" "gcc" "CMakeFiles/TRABALHO.dir/Calculadora.cpp.obj.d"
+  "C:/Users/Eu/Documents/matriz-de-distancias/GeradorImagem.cpp" "CMakeFiles/TRABALHO.dir/GeradorImagem.cpp.obj" "gcc" "CMakeFiles/TRABALHO.dir/GeradorImagem.cpp.obj.d"
+  "C:/Users/Eu/Documents/matriz-de-distancias/Impressora.cpp" "CMakeFiles/TRABALHO.dir/Impressora.cpp.obj" "gcc" "CMakeFiles/TRABALHO.dir/Impressora.cpp.obj.d"
+  "C:/Users/Eu/Documents/matriz-de-distancias/Pixel.cpp" "CMakeFiles/TRABALHO.dir/Pixel.cpp.obj" "gcc" "CMakeFiles/TRABALHO.dir/Pixel.cpp.obj.d"
+  "C:/Users/Eu/Documents/matriz-de-distancias/main.cpp" "CMakeFiles/TRABALHO.dir/main.cpp.obj" "gcc" "CMakeFiles/TRABALHO.dir/main.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
--- cmake-build-debug/cmake_install.cmake
+++ cmake-build-debug/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: C:/Users/Eu/Documents/TRABALHO
+# Install script for directory: C:/Users/Eu/Documents/matriz-de-distancias
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -45,5 +45,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "C:/Users/Eu/Documents/TRABALHO/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "C:/Users/Eu/Documents/matriz-de-distancias/cmake-build-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
